a34de942c1728628b45655dff68f8e824cd54439
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-03-09 19:39:10 +0100

    Add project files.

---
+++ WebApp/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+public class AuthController : Controller
+{
+    public IActionResult Register()
+    {
+        return View();
+    }
+    public IActionResult Login()
+    {
+        return View();
+    }
+}
---
+++ WebApp/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+
+
+
+
+
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+}
+
+app.UseHsts();
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Projects}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
